0e75145be234c26eb52056e167d8f2a11eab4b7a
Maximilian Heymann <maxhey4@gmail.com>
2022-07-06 11:58:42 +0200

    fix build files

--- build.bat
+++ build.bat
@@ -1,4 +1,4 @@
 @echo off
-set name=CIS Benchmark Converter
+set name=CIS-Benchmark-Converter
 .\venv\Scripts\pyinstaller.exe --clean -F -i .\favicon.ico -n "%name%" .\main.py
 DEL "%name%.spec"
--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-name="CIS Benchmark Converter"
-./venv/bin/pyinstaller -clean -F -i ./favicon.ico -n "$name" ./main.py
+name="CIS-Benchmark-Converter"
+./venv/bin/pyinstaller --clean -F -i ./favicon.ico -n "$name" ./main.py
 cp "./dist/$name" ./
 rm "$name.spec"
