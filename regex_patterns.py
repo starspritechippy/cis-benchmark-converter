a2f428b9393e2177e4bd74c195294d0c071f95db
Maximilian Heymann <maxhey4@gmail.com>
2022-07-06 09:21:54 +0200

    improve comments

--- regex_patterns.py
+++ regex_patterns.py
@@ -1,10 +1,11 @@
 import re
 
 # Matches, if the line starts with any dot-separated numbers, 
-# and has two consecutive dots after the bullet point
+# and has a number at the end
 # i.e. 12.1.2
 # Group 1: dot-separated numbers
 # Group 2: subject line
+# Group 3: Page Number
 dot_separated_pattern = re.compile(r"^([\d+\.]*\d+) (.*?) ?(\d+) ?$")
 
 # Matches if the end of a line features a page number
